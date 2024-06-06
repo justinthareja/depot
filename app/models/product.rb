e205d2f68fdffbe98eb27729078be2b89ca119e3
Justin <justin.thareja@gmail.com>
2024-06-06 15:46:02 -0600

    add stricter validation for product title

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
-class Product < ApplicationRecord
+class Product < ApplicationRecord # rubocop:disable Style/Documentation,Style/FrozenStringLiteralComment
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
@@ -6,4 +6,7 @@ class Product < ApplicationRecord
     with: /\.(gif|jpg|png)\z/i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+  # JT: allow_blank: true means that the validation will only be run if the field is not blank
+  # this will avoid getting multiple error messages when the title is blank
+  validates :title, allow_blank: true, length: { minimum: 10 }
 end

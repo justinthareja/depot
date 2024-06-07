142269c57253a5d8b3b12a94416ce1460a2f508e
Justin <justin.thareja@gmail.com>
2024-06-07 14:57:25 -0600

    update store controller test to include assertions for HTML elements in the response body

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,8 +1,14 @@
-require "test_helper"
+require 'test_helper' # rubocop:disable Style/FrozenStringLiteralComment
 
 class StoreControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get store_index_url
     assert_response :success
+    # JT: assert_select takes a look into the response body and checks if it contains the specified HTML
+    # elements or attributes. It allows you to perform assertions on the HTML structure of the page.
+    assert_select 'nav a', minimum: 4
+    assert_select 'main ul li', 3
+    assert_select 'h2', 'Programming Ruby 1.9'
+    assert_select 'div', /\$[,\d]+\.\d\d/
   end
 end

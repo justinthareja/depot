124ced74ee57809c2b8d12c2af86532914146c84
Justin <justin.thareja@gmail.com>
2024-06-12 09:44:26 -0600

    fix bug in cart model passing incorrect arguments to find_by_product_id

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,10 @@
 class Cart < ApplicationRecord
   # JT: the dependent: :destroy option tells rails to delete any associated line items when the cart is deleted
-  # this allows rails to reference line items through the cart object
+  # this association adds several methods to the Cart model including the line_items method
   has_many :line_items, dependent: :destroy
 
   def add_product(product)
-    current_item = line_items.find_by_product_id(product_id: product.id)
+    current_item = line_items.find_by_product_id(product.id)
     if current_item
       current_item.quantity += 1
     else

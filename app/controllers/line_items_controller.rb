b3b464ffe91cf88f9570b32a4c61a97c574c4618
Justin <justin.thareja@gmail.com>
2024-06-13 08:53:56 -0600

    delete individual line items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -55,10 +55,10 @@ class LineItemsController < ApplicationController
 
   # DELETE /line_items/1 or /line_items/1.json
   def destroy
-    @line_item.destroy!
+    @line_item.destroy! if line_item.cart.id == session[:cart_id]
 
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      # format.html { redirect_to store_index_url, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
